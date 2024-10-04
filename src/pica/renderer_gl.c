78f1fbaafb5f7e6e1ef94b54c7bdb7afb87b1e6d
burhanr13 <burhan.rajgara@gmail.com>
2024-10-04 16:09:02 -0400

    bruh

--- src/pica/renderer_gl.c
+++ src/pica/renderer_gl.c
@@ -140,6 +140,8 @@ void renderer_gl_setup(GLState* state) {
         glBindTexture(GL_TEXTURE_2D, state->fbs[i].tex_colorbuf);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, SCREEN_WIDTH, SCREEN_HEIGHT, 0,
                      GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                                GL_TEXTURE_2D, state->fbs[i].tex_colorbuf, 0);
 
@@ -162,7 +164,6 @@ void render_gl_main(GLState* state) {
     glUseProgram(state->main.program);
     glBindVertexArray(state->main.vao);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
 #ifdef WIREFRAME
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 #endif
